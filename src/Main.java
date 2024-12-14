a35143d7c851bcff48d6647bae3ee3d80a5cfd12
Mert Mücahid Yurtseven <mucahidyurtseven@gmail.com>
2024-12-14 19:56:58 +0300

    IYI INSANLAR IYI KI VAR

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("TO-DO-LIST");
+        System.out.println("TO-DO-LIST"); //bu basligi atmasaniz napardik ya iyi insanlar iyi ki var
 
     }
 
