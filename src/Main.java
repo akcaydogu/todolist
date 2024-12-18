5afd292179f6840adf199538164255d93872ffd3
Doğukan Akçay <akcay.dogukan61@gmail.com>
2024-12-19 02:27:53 +0300

    Fixes and optimisation

--- src/Main.java
+++ src/Main.java
@@ -3,19 +3,186 @@ import java.util.HashMap;
 import java.util.Scanner;
 
 public class Main {
-    public static HashMap<Integer, String> kategoriHash = new HashMap<Integer, String>();
+//    public static HashMap<Integer, String> kategoriHash = new HashMap<Integer, String>(); Mert
+    public static ArrayList<String> categoryList = new ArrayList<>();
     public static int a = 4;
 
-    public static void main(String[] args) {
+//    public static void main(String[] args) {
+//
+//        Kategori(); Mert
+//    }
 
-        Kategori();
+    public static void main(String[] args) {
+        System.out.println("TO-DO-LIST");
+
+        Scanner scanner = new Scanner(System.in);
+        HashMap<Integer, ArrayList<String>> gorevlerHashMap = new HashMap<>();
+        HashMap<Integer, ArrayList<String>> coplerHashMap = new HashMap<>();
+        ArrayList<String> gorevler = new ArrayList<>();
+        ArrayList<String> copler = new ArrayList<>();
+        categoryList.add("İş");
+        categoryList.add("Okul");
+        categoryList.add("Günlük Yaşam");
+
+
+        while (true) {
+            Giris();
+
+            int tercih = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (tercih) {
+                case 1:
+                    GorevEkleme(scanner,gorevlerHashMap);
+                    break;
+                case 2:
+                    GorevSilme(scanner,gorevlerHashMap,coplerHashMap);
+                    break;
+                case 3:
+                    GorevGoruntuleme(gorevlerHashMap);
+                    break;
+                case 4:
+                    CopKutusu(coplerHashMap);
+                    break;
+                case 5:
+                    GorevGeriGetirme(scanner,gorevlerHashMap,coplerHashMap);
+                    break;
+                case 6:
+                    Kategori();
+                    break;
+                default:
+                    System.out.println("Gecersiz bir deger girdiniz.1,2,3,4,5 veya 6 değerini giriniz.");
+            }
+        }
+    }
+    public static void Giris(){
+        System.out.println("Görev eklemek için 1'e basınız.");
+        System.out.println("Gorev silmek için 2'ye basınız.");
+        System.out.println("Görevleri goruntulemek icin 3'e basiniz.");
+        System.out.println("Çöp kutusunu görüntülemek için 4'e basiniz.");
+        System.out.println("Çöp kutusundan bir görev çıkartmak için 5'e basınız.");
+        System.out.println("Kategori için 6'ya basınız.");
+    }
+    public static void GorevEkleme(Scanner scanner,HashMap<Integer, ArrayList<String>> gorevlerHashMap){
+        System.out.println("Bir gorev ekleyiniz.");
+        String gorev = scanner.nextLine();
+        System.out.println("Bir kategori seçiniz ");
+        for (int i = 0; i < categoryList.size(); i++) {
+            System.out.println("\t" + categoryList.get(i));
+        }
+        System.out.println("Kategori eklemek istemiyorsanız boş bırakınız");
+        String category = scanner.nextLine();
+        if (isCategoryValid(category) || category.isEmpty()) {
+            gorevlerHashMap.put(gorevlerHashMap.size()+1,new ArrayList<String>());
+            gorevlerHashMap.get(gorevlerHashMap.size()).add(gorev);
+            gorevlerHashMap.get(gorevlerHashMap.size()).add(category);
+            gorevlerHashMap.get(gorevlerHashMap.size()).add("0");
+        } else {
+            System.out.println("Geçersiz kategori lütfen kategori ismini doğru yazdığınızdan emin olun!");
+        }
+
+//        gorevler.add(scanner.nextLine()); Aylin
+
+    }
+    public static void GorevSilme(Scanner scanner,HashMap<Integer, ArrayList<String>> gorevlerHashMap,HashMap<Integer, ArrayList<String>> coplerHashMap){
+//        System.out.println("Silmek istediğiniz görevi giriniz.");
+        if (gorevlerHashMap.isEmpty()) {
+            System.out.println("Silinecek görev bulunmuyor.");
+        } else {
+            System.out.println("Silmek istediğiniz görevi seçiniz:");
+            System.out.printf("%s %-30s %-10s %-10s %n", "N", "Görev", "Kategori", "Yapılma durumu");
+        }
+        for (Integer x : gorevlerHashMap.keySet()) {
+            String gorevName = gorevlerHashMap.get(x).get(0);
+            String category = gorevlerHashMap.get(x).get(1);
+            String isDone = gorevlerHashMap.get(x).get(2);
+            System.out.printf("%d %-30s %-10s %-10s %n", x, gorevName, category, isDone);
+        }
+//        for (int i = 0; i < gorevler.size(); i++) {
+//            System.out.println((i) + ". " + gorevler.get(i));   Aylin
+//        }
+        int index = scanner.nextInt();
+        if (index >= 0 && index <= gorevlerHashMap.size()) {
+            coplerHashMap.put(coplerHashMap.size()+1,new ArrayList<String>());
+            coplerHashMap.get(coplerHashMap.size()).add(gorevlerHashMap.get(index).get(0));
+            coplerHashMap.get(coplerHashMap.size()).add(gorevlerHashMap.get(index).get(1));
+            coplerHashMap.get(coplerHashMap.size()).add(gorevlerHashMap.get(index).get(2));
+            gorevlerHashMap.remove(index);
+            System.out.println("Görev çöpe atıldı");
+        }
+
+    }
+
+    public static void GorevGoruntuleme(HashMap<Integer, ArrayList<String>> gorevlerHashMap){
+//        System.out.println("GÖREVLER:");
+//        System.out.println(gorevler); Aylin
+        System.out.printf("%s %-30s %-10s %-10s %n", "N", "Görev", "Kategori", "Yapılma durumu");
+        if (gorevlerHashMap.isEmpty()) {
+            System.out.printf("%s%n", "Boş");
+        }
+        for (Integer x : gorevlerHashMap.keySet()) {
+            String gorevName = gorevlerHashMap.get(x).get(0);
+            String category = gorevlerHashMap.get(x).get(1);
+            String isDone = gorevlerHashMap.get(x).get(2);
+            System.out.printf("%d %-30s %-10s %-10s %n", x, gorevName, category, isDone);
+        }
+
+    }
+    public static void CopKutusu(HashMap<Integer, ArrayList<String>> coplerHashMap){
+        if (coplerHashMap.isEmpty()) {
+            System.out.println("Çöp kutusu boş.");
+        } else {
+            System.out.println("ÇÖP KUTUSU:");
+            System.out.printf("%s %-30s %-10s %-10s %n", "N", "Görev", "Kategori", "Yapılma durumu");
+            for (Integer x : coplerHashMap.keySet()) {
+                String gorevName = coplerHashMap.get(x).get(0);
+                String category = coplerHashMap.get(x).get(1);
+                String isDone = coplerHashMap.get(x).get(2);
+                System.out.printf("%d %-30s %-10s %-10s %n", x, gorevName, category, isDone);
+            }
+        }
+
+
+    }
+    public static void GorevGeriGetirme(Scanner scanner,HashMap<Integer, ArrayList<String>> gorevlerHashMap,HashMap<Integer, ArrayList<String>> coplerHashMap){
+        if (coplerHashMap.isEmpty()) {
+            System.out.println("Çöp kutusu boş.");
+        } else {
+            System.out.println("Çöp kutusundan çıkarmak istediğiniz görevi yazınız");
+        }
+//        for (int i = 0; i <= coplerHashMap.size(); i++) {
+//            System.out.println((i) + ". " + copler.get(i));
+//            int index2 = scanner.nextInt();
+//            if (index2 >= 0 && index2 < copler.size()) {
+//                String geriGetirilenGorev = copler.remove(index2);
+//                gorevler.add(geriGetirilenGorev);
+//                System.out.println("Görev çöpten çıkarıldı");
+//            }
+//        }
+        System.out.printf("%s %-30s %-10s %-10s %n", "N", "Görev", "Kategori", "Yapılma durumu");
+        for (Integer x : coplerHashMap.keySet()) {
+            String gorevName = coplerHashMap.get(x).get(0);
+            String category = coplerHashMap.get(x).get(1);
+            String isDone = coplerHashMap.get(x).get(2);
+            System.out.printf("%d %-30s %-10s %-10s %n", x, gorevName, category, isDone);
+        }
+        int index = scanner.nextInt();
+        if (index >= 0 && index <= gorevlerHashMap.size()) {
+            gorevlerHashMap.put(gorevlerHashMap.size()+1,new ArrayList<String>());
+            gorevlerHashMap.get(gorevlerHashMap.size()).add(coplerHashMap.get(index).get(0));
+            gorevlerHashMap.get(gorevlerHashMap.size()).add(coplerHashMap.get(index).get(1));
+            gorevlerHashMap.get(gorevlerHashMap.size()).add(coplerHashMap.get(index).get(2));
+            coplerHashMap.remove(index);
+            System.out.println("Görev çöpten çıkarıldı");
+        }
+
     }
 
     public static void Kategori() {
 
-        kategoriHash.put(1, "İş");
-        kategoriHash.put(2, "Okul");
-        kategoriHash.put(3, "Günlük Yaşam");
+//        kategoriHash.put(1, "İş");
+//        kategoriHash.put(2, "Okul");
+//        kategoriHash.put(3, "Günlük Yaşam");
 
         Scanner scanner = new Scanner(System.in);
         boolean baskaislem = true;
@@ -45,57 +212,48 @@ public class Main {
                 continue;
             }
         }
+    }
 
+    private static boolean isCategoryValid(String category) {
+        return categoryList.contains(category);
     }
 
     public static void KategoriEkleme() {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("Gorevi Hangi Kategoriye Kaydetmek Istersiniz ?\n1-" + kategoriHash.get(1) +
-                "\n2-" + kategoriHash.get(2) + "\n3-" + kategoriHash.get(3) + "\n4-Yeni Kategori Olustur");
-        int hangiKategoriye = scanner.nextInt();
-        scanner.nextLine();
-        switch (hangiKategoriye) {
-            case 1:
-                System.out.println("Goreviniz " + kategoriHash.get(1) + " Adli Kategoriye Basariyla Eklendi");
-                break;
-            case 2:
-                System.out.println("Goreviniz " + kategoriHash.get(2) + " Adli Kategoriye Basariyla Eklendi");
-                break;
-            case 3:
-                System.out.println("Goreviniz " + kategoriHash.get(3) + " Adli Kategoriye Basariyla Eklendi");
-                break;
-            case 4:
-                boolean kategorieklemebabapro = true;
+//        System.out.println("Gorevi Hangi Kategoriye Kaydetmek Istersiniz ?\n1-" + kategoriHash.get(1) +
+//                "\n2-" + kategoriHash.get(2) + "\n3-" + kategoriHash.get(3) + "\n4-Yeni Kategori Olustur");
 
-                while (kategorieklemebabapro) {
+        System.out.println("Yeni kategori ismini giriniz.");
 
-                    for (int i = a; i > kategoriHash.size(); i += 0) {
+        boolean kategorieklemebabapro = true;
 
-                        System.out.println("Ekleyeceginiz Kategorinin Adi Ne Olsun");
-                        String kategoriAdi = scanner.nextLine();
-                        kategoriHash.put(i, kategoriAdi);
-                        System.out.println(kategoriHash.get(i) + " Adli Kategori Basariyla Eklendi");
-                    }
-                    a++;
-                    boolean baskaKategoriEkleme = true;
-                    while (baskaKategoriEkleme) {
-                        System.out.println("Baska bir kategori eklemek ister misiniz ? \n1-Evet \n2-Hayir");
+        while (kategorieklemebabapro) {
 
-                        int baskaKategoriIsterMisiniz = scanner.nextInt();
-                        scanner.nextLine();
-                        if (baskaKategoriIsterMisiniz == 1) {
-                            break;
-                        } else if (baskaKategoriIsterMisiniz == 2) {
-                            kategorieklemebabapro = false;
-                            break;
-                        } else {
-                            System.out.println("Hatali giris lutfen tekrar giriniz!!!");
-                            continue;
-                        }
-                    }
+
+            System.out.println("Ekleyeceginiz Kategorinin Adi Ne Olsun");
+            String kategoriAdi = scanner.nextLine();
+            categoryList.add(kategoriAdi);
+            System.out.println(kategoriAdi + " Adli Kategori Basariyla Eklendi");
+
+            boolean baskaKategoriEkleme = true;
+            while (baskaKategoriEkleme) {
+                System.out.println("Baska bir kategori eklemek ister misiniz ? \n1-Evet \n2-Hayir");
+
+                int baskaKategoriIsterMisiniz = scanner.nextInt();
+                scanner.nextLine();
+                if (baskaKategoriIsterMisiniz == 1) {
+                    break;
+                } else if (baskaKategoriIsterMisiniz == 2) {
+                    kategorieklemebabapro = false;
+                    break;
+                } else {
+                    System.out.println("Hatali giris lutfen tekrar giriniz!!!");
+                    continue;
                 }
+            }
         }
+
     }
 
     public static void KategoriCikarma() {
@@ -105,15 +263,15 @@ public class Main {
             System.out.println("Silmek istediginiz kategorinin numarasini giriniz : ");
             KategoriGoruntuleme();
             int kategoriCikarma = scanner.nextInt();
-            if (kategoriCikarma <= kategoriHash.size()) {
-                kategoriHash.remove(kategoriCikarma);
+            if (kategoriCikarma <= categoryList.size()) {
+                categoryList.remove(kategoriCikarma);
                 System.out.println("Kategori basariyla kaldirildi.");
                 System.out.println("Baska bir kategori silmek istiyor musunuz ?\n1-Evet \n2-Hayir");
                 int baskaSilme = scanner.nextInt();
                 if (baskaSilme == 1) {
                     KategoriCikarma();
                 } else if (baskaSilme == 2) {
-                   break;
+                    break;
                 } else {
                     System.out.println("Hatali giris lutfen tekrar giriniz!!!");
                 }
@@ -124,126 +282,18 @@ public class Main {
     }
 
     public static void KategoriGoruntuleme() {
-        System.out.println(kategoriHash);
+//        System.out.println(kategoriHash); Mert
+        for (int i = 0; i < categoryList.size(); i++) {
+            System.out.println(i+". "+categoryList.get(i));
+        }
     }
 
-    public static void KategoriFiltreleme() {
+    public static void KategoriFiltreleme(String category) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Hangi numarali kategorinin icerigini gormek istiyorsunuz ?");
         KategoriGoruntuleme();
         int kategorigoruntuleme = scanner.nextInt();
-
     }
 
 }
-import java.util.ArrayList;
-import java.util.Scanner;
 
-
-public class main {
-
-    public static void main(String[] args) {
-        System.out.println("TO-DO-LIST");
-
-        Scanner scanner = new Scanner(System.in);
-        ArrayList<String> gorevler = new ArrayList<>();
-        ArrayList<String> copler = new ArrayList<>();
-
-
-        while (true) {
-            Giris();
-
-
-            int tercih = scanner.nextInt();
-
-
-            switch (tercih) {
-                case 1:
-                    GorevEkleme(scanner,gorevler);
-                    break;
-                case 2:
-                    GorevSilme(scanner,gorevler,copler);
-                    break;
-                case 3:
-                    GorevGoruntuleme(gorevler);
-                    break;
-                case 4:
-                    CopKutusu(copler);
-                    break;
-                case 5:
-                    GorevGeriGetirme(scanner,copler,gorevler);
-                    break;
-                default:
-                    System.out.println("Gecersiz bir deger girdiniz.1,2,3,4 veya 5 değerini giriniz.");
-
-
-
-            }
-        }
-    }
-
-    public static void Giris(){
-        System.out.println("Görev eklemek için 1'e basınız.");
-        System.out.println("Gorev silmek için 2'ye basınız.");
-        System.out.println("Görevleri goruntulemek icin 3'e basiniz.");
-        System.out.println("Çöp kutusunu görüntülemek için 4'e basiniz.");
-        System.out.println("Çöp kutusundan bir görev çıkartmak için 5'e basınız.");
-    }
-    public static void GorevEkleme(Scanner scanner,ArrayList<String> gorevler){
-        System.out.println("Bir gorev ekleyiniz.");
-        scanner.nextLine();
-        gorevler.add(scanner.nextLine());
-    }
-    public static void GorevSilme(Scanner scanner,ArrayList<String> gorevler,ArrayList<String> copler){
-        System.out.println("Silmek istediğiniz görevi giriniz.");
-        if (gorevler.isEmpty()) {
-            System.out.println("Silinecek görev bulunmuyor.");
-        } else {
-            System.out.println("Silmek istediğiniz görevi seçiniz:");
-        }
-        for (int i = 0; i < gorevler.size(); i++) {
-            System.out.println((i) + ". " + gorevler.get(i));
-        }
-        int index = scanner.nextInt();
-        if (index >= 0 && index < gorevler.size()) {
-            String silinenGorev = gorevler.remove(index);
-            copler.add(silinenGorev);
-            System.out.println("Görev çöpe atıldı");
-        }
-
-    }
-
-    public static void GorevGoruntuleme(ArrayList<String> gorevler){
-        System.out.println("GÖREVLER:");
-        System.out.println(gorevler);
-    }
-    public static void CopKutusu( ArrayList<String> copler){
-        if (copler.isEmpty()) {
-            System.out.println("Çöp kutusu boş.");
-        } else {
-            System.out.println("ÇÖP KUTUSU:");
-            System.out.println(copler);
-        }
-
-
-    }
-    public static void GorevGeriGetirme(Scanner scanner,ArrayList<String> copler,ArrayList<String> gorevler){
-        if (copler.isEmpty()) {
-            System.out.println("Çöp kutusu boş.");
-        } else {
-            System.out.println("Çöp kutusundan çıkarmak istediğiniz görevi yazınız");
-        } for (int i = 0; i < copler.size(); i++) {
-            System.out.println((i) + ". " + copler.get(i));
-            int index2 = scanner.nextInt();
-            if (index2 >= 0 && index2 < copler.size()) {
-                String geriGetirilenGorev = copler.remove(index2);
-                gorevler.add(geriGetirilenGorev);
-                System.out.println("Görev çöpten çıkarıldı");
-            }
-        }
-
-    }
-
-
-
-}
